6e6f7900ad494d58dcf01b06ff52b6465c4b45d2
Harshitha NC <nc.harshitha30@gmail.com>
2026-04-14 14:01:10 +0530

    Fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lv#+w5&mq!xqe(ng)6ji4rlsf0#f&#@(#1_^g!#r+k$)879yst'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['employee-portal.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -117,8 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
